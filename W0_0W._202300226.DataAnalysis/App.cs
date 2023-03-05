53aca13918661bbde89ac08d731866899d9ac7f5
qingxu4935 <qingxu4935@163.com>
2023-03-05 11:57:57 +0800

    App.Init

---
+++ W0_0W._202300226.DataAnalysis/App.cs
@@ -0,0 +1,13 @@
+﻿using Splat;
+
+namespace W0_0W._202300226.DataAnalysis
+{
+	static class App
+	{
+		public static void Init()
+		{
+			var build = Locator.CurrentMutable;
+			build.RegisterLazySingleton(() => new Config());
+		}
+	}
+}
---
+++ W0_0W._202300226.DataAnalysis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace W0_0W._202300226.DataAnalysis
+{
+	internal static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			App.Init();
+			Application.Run(new Form());
+		}
+	}
+}
