fbcc4376ee94d6beec7ba718e4d2c2fb1af21286
qingxu4935 <qingxu4935@163.com>
2023-03-11 23:33:33 +0800

    ShowProgress

--- W0_0W._202300226.DataAnalysis/Form.cs
+++ W0_0W._202300226.DataAnalysis/Form.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Reactive.Disposables;
 using System.Windows.Forms;
 using DevExpress.XtraCharts.Printing;
 using DevExpress.XtraPrinting;
@@ -21,35 +23,24 @@ namespace W0_0W._202300226.DataAnalysis
 			{
 				var fileName = xtraOpenFileDialog.FileName;
 
-				var progressPanelHandle = ShowProgressPanel();
-
-				var signalRiver = new SignalRiver(Locator.Current.GetService<Config>());
-				signalRiver.Load(fileName);
-
-				var series = chart.Series[0];
-				series.DataSource = signalRiver.Signals;
-				series.ArgumentDataMember = nameof(Signal.Second);
-				series.ValueDataMembers[0] = nameof(Signal.Value);
-
-				chart.Titles[0].Text = $"#{signalRiver.DeviceName} - 峰值: {signalRiver.MaxValue}";
-				statusText.Caption = fileName;
-				if (parametersView.Visible)
+				using (ShowProgress())
 				{
-					parametersView.Visible = false;
+					var signalRiver = new SignalRiver(Locator.Current.GetService<Config>());
+					signalRiver.Load(fileName);
+
+					var series = chart.Series[0];
+					series.DataSource = signalRiver.Signals;
+					series.ArgumentDataMember = nameof(Signal.Second);
+					series.ValueDataMembers[0] = nameof(Signal.Value);
+
+					chart.Titles[0].Text = $"#{signalRiver.DeviceName} - 峰值: {signalRiver.MaxValue}";
+					statusText.Caption = fileName;
+					if (parametersView.Visible)
+					{
+						parametersView.Visible = false;
+					}
 				}
-
-				CloseProgressPanel(progressPanelHandle);
 			}
-		}
-
-		IOverlaySplashScreenHandle ShowProgressPanel()
-		{
-			return SplashScreenManager.ShowOverlayForm(this, OverlayWindowOptions.Default);
-		}
-
-		void CloseProgressPanel(IOverlaySplashScreenHandle handle)
-		{
-			SplashScreenManager.CloseOverlayForm(handle);
 		}
 
 		void Preview_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
@@ -63,33 +54,34 @@ namespace W0_0W._202300226.DataAnalysis
 			{
 				chart.OptionsPrint.SizeMode = PrintSizeMode.Zoom;
 				var filename = xtraSaveFileDialog.FileName;
-				var ext = Path.GetExtension(filename);
-				var progressPanelHandle = ShowProgressPanel();
-				switch (ext)
+				var extension = Path.GetExtension(filename);
+				using (ShowProgress())
 				{
-					case ".pdf":
-						{
-							chart.OptionsPrint.ImageFormat = PrintImageFormat.Metafile;
-							var options = new PdfExportOptions
+					switch (extension)
+					{
+						case ".pdf":
 							{
-								ConvertImagesToJpeg = false
-							};
+								chart.OptionsPrint.ImageFormat = PrintImageFormat.Metafile;
+								var options = new PdfExportOptions
+								{
+									ConvertImagesToJpeg = false
+								};
 
-							chart.ExportToPdf(filename, options);
+								chart.ExportToPdf(filename, options);
 
+								break;
+							}
+						case ".xls":
+							chart.ExportToXls(filename);
 							break;
-						}
-					case ".xls":
-						chart.ExportToXls(filename);
-						break;
-					case ".xlsx":
-						chart.ExportToXlsx(filename);
-						break;
-					case ".docx":
-						chart.ExportToDocx(filename);
-						break;
+						case ".xlsx":
+							chart.ExportToXlsx(filename);
+							break;
+						case ".docx":
+							chart.ExportToDocx(filename);
+							break;
+					}
 				}
-				CloseProgressPanel(progressPanelHandle);
 			}
 		}
 
@@ -97,6 +89,12 @@ namespace W0_0W._202300226.DataAnalysis
 		{
 			parametersView.Visible = true;
 			parametersView.BringToFront();
+		}
+
+		IDisposable ShowProgress()
+		{
+			var progressPanelHandle = SplashScreenManager.ShowOverlayForm(this, OverlayWindowOptions.Default);
+			return Disposable.Create(() => SplashScreenManager.CloseOverlayForm(progressPanelHandle));
 		}
 	}
 }
