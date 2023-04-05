fef7d2c021489b96d0d4e2fbdc20e6fefd4df447
qingxu4935 <qingxu4935@163.com>
2023-04-05 22:19:03 +0800

    精简主界面

--- W0_0W._202300226.DataAnalysis/Form.Designer.cs
+++ W0_0W._202300226.DataAnalysis/Form.Designer.cs
@@ -42,24 +42,17 @@
 			DevExpress.XtraCharts.ChartTitle chartTitle1 = new DevExpress.XtraCharts.ChartTitle();
 			barManager1 = new DevExpress.XtraBars.BarManager(components);
 			bar1 = new DevExpress.XtraBars.Bar();
-			Preview = new DevExpress.XtraBars.BarButtonItem();
-			Export = new DevExpress.XtraBars.BarButtonItem();
 			Paramters = new DevExpress.XtraBars.BarButtonItem();
-			bar2 = new DevExpress.XtraBars.Bar();
-			barSubItem1 = new DevExpress.XtraBars.BarSubItem();
-			OpenHistoryFile = new DevExpress.XtraBars.BarButtonItem();
-			barSubItem3 = new DevExpress.XtraBars.BarSubItem();
-			barSubItem5 = new DevExpress.XtraBars.BarSubItem();
+			openData = new DevExpress.XtraBars.BarButtonItem();
+			Preview = new DevExpress.XtraBars.BarButtonItem();
 			bar3 = new DevExpress.XtraBars.Bar();
 			statusText = new DevExpress.XtraBars.BarStaticItem();
 			barDockControlTop = new DevExpress.XtraBars.BarDockControl();
 			barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
 			barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
 			barDockControlRight = new DevExpress.XtraBars.BarDockControl();
-			barButtonItem5 = new DevExpress.XtraBars.BarButtonItem();
 			chart = new DevExpress.XtraCharts.ChartControl();
 			xtraOpenFileDialog = new DevExpress.XtraEditors.XtraOpenFileDialog(components);
-			xtraSaveFileDialog = new DevExpress.XtraEditors.XtraSaveFileDialog(components);
 			((System.ComponentModel.ISupportInitialize)barManager1).BeginInit();
 			((System.ComponentModel.ISupportInitialize)chart).BeginInit();
 			((System.ComponentModel.ISupportInitialize)xyDiagram1).BeginInit();
@@ -77,41 +70,24 @@
 			// 
 			barManager1.AllowCustomization = false;
 			barManager1.AllowQuickCustomization = false;
-			barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] { bar1, bar2, bar3 });
+			barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] { bar1, bar3 });
 			barManager1.DockControls.Add(barDockControlTop);
 			barManager1.DockControls.Add(barDockControlBottom);
 			barManager1.DockControls.Add(barDockControlLeft);
 			barManager1.DockControls.Add(barDockControlRight);
 			barManager1.Form = this;
-			barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] { barSubItem1, barSubItem3, OpenHistoryFile, barButtonItem5, barSubItem5, statusText, Preview, Export, Paramters });
-			barManager1.MainMenu = bar2;
-			barManager1.MaxItemId = 20;
+			barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] { statusText, Preview, Paramters, openData });
+			barManager1.MaxItemId = 21;
 			barManager1.StatusBar = bar3;
 			// 
 			// bar1
 			// 
 			bar1.BarName = "Tools";
 			bar1.DockCol = 0;
-			bar1.DockRow = 1;
+			bar1.DockRow = 0;
 			bar1.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
-			bar1.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] { new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, Preview, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph), new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, Export, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph), new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, Paramters, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph) });
+			bar1.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] { new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, Paramters, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph), new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, openData, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph), new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, Preview, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph) });
 			bar1.Text = "Tools";
-			// 
-			// Preview
-			// 
-			Preview.Caption = "预览";
-			Preview.Id = 17;
-			Preview.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("Preview.ImageOptions.SvgImage");
-			Preview.Name = "Preview";
-			Preview.ItemClick += Preview_ItemClick;
-			// 
-			// Export
-			// 
-			Export.Caption = "导出";
-			Export.Id = 18;
-			Export.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("Export.ImageOptions.SvgImage");
-			Export.Name = "Export";
-			Export.ItemClick += Export_ItemClick;
 			// 
 			// Paramters
 			// 
@@ -121,45 +97,21 @@
 			Paramters.Name = "Paramters";
 			Paramters.ItemClick += Parameters_ItemClick;
 			// 
-			// bar2
+			// openData
 			// 
-			bar2.BarName = "Main menu";
-			bar2.DockCol = 0;
-			bar2.DockRow = 0;
-			bar2.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
-			bar2.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] { new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, barSubItem1, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph), new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, barSubItem3, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph) });
-			bar2.OptionsBar.MultiLine = true;
-			bar2.OptionsBar.UseWholeRow = true;
-			bar2.Text = "Main menu";
+			openData.Caption = "打开历史数据";
+			openData.Id = 20;
+			openData.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("openData.ImageOptions.SvgImage");
+			openData.Name = "openData";
+			openData.ItemClick += OpenData_ItemClick;
 			// 
-			// barSubItem1
+			// Preview
 			// 
-			barSubItem1.Caption = "数据获取";
-			barSubItem1.Id = 0;
-			barSubItem1.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("barSubItem1.ImageOptions.SvgImage");
-			barSubItem1.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] { new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, OpenHistoryFile, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph) });
-			barSubItem1.Name = "barSubItem1";
-			// 
-			// OpenHistoryFile
-			// 
-			OpenHistoryFile.Caption = "打开历史数据";
-			OpenHistoryFile.Id = 4;
-			OpenHistoryFile.Name = "OpenHistoryFile";
-			OpenHistoryFile.ItemClick += OpenHistoryFile_ItemClick;
-			// 
-			// barSubItem3
-			// 
-			barSubItem3.Caption = "数据分析";
-			barSubItem3.Id = 2;
-			barSubItem3.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("barSubItem3.ImageOptions.SvgImage");
-			barSubItem3.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] { new DevExpress.XtraBars.LinkPersistInfo(barSubItem5) });
-			barSubItem3.Name = "barSubItem3";
-			// 
-			// barSubItem5
-			// 
-			barSubItem5.Caption = "定标";
-			barSubItem5.Id = 15;
-			barSubItem5.Name = "barSubItem5";
+			Preview.Caption = "报告";
+			Preview.Id = 17;
+			Preview.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("Preview.ImageOptions.SvgImage");
+			Preview.Name = "Preview";
+			Preview.ItemClick += Preview_ItemClick;
 			// 
 			// bar3
 			// 
@@ -187,7 +139,7 @@
 			barDockControlTop.Location = new System.Drawing.Point(0, 0);
 			barDockControlTop.Manager = barManager1;
 			barDockControlTop.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-			barDockControlTop.Size = new System.Drawing.Size(826, 49);
+			barDockControlTop.Size = new System.Drawing.Size(826, 24);
 			// 
 			// barDockControlBottom
 			// 
@@ -202,25 +154,19 @@
 			// 
 			barDockControlLeft.CausesValidation = false;
 			barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
-			barDockControlLeft.Location = new System.Drawing.Point(0, 49);
+			barDockControlLeft.Location = new System.Drawing.Point(0, 24);
 			barDockControlLeft.Manager = barManager1;
 			barDockControlLeft.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-			barDockControlLeft.Size = new System.Drawing.Size(0, 478);
+			barDockControlLeft.Size = new System.Drawing.Size(0, 503);
 			// 
 			// barDockControlRight
 			// 
 			barDockControlRight.CausesValidation = false;
 			barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
-			barDockControlRight.Location = new System.Drawing.Point(826, 49);
+			barDockControlRight.Location = new System.Drawing.Point(826, 24);
 			barDockControlRight.Manager = barManager1;
 			barDockControlRight.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-			barDockControlRight.Size = new System.Drawing.Size(0, 478);
-			// 
-			// barButtonItem5
-			// 
-			barButtonItem5.Caption = "定标";
-			barButtonItem5.Id = 8;
-			barButtonItem5.Name = "barButtonItem5";
+			barDockControlRight.Size = new System.Drawing.Size(0, 503);
 			// 
 			// chart
 			// 
@@ -246,7 +192,7 @@
 			chart.Dock = System.Windows.Forms.DockStyle.Fill;
 			chart.Legend.MarkerMode = DevExpress.XtraCharts.LegendMarkerMode.CheckBoxAndMarker;
 			chart.Legend.Visibility = DevExpress.Utils.DefaultBoolean.True;
-			chart.Location = new System.Drawing.Point(0, 49);
+			chart.Location = new System.Drawing.Point(0, 24);
 			chart.Name = "chart";
 			series1.Name = "信号量";
 			series1.View = lineSeriesView1;
@@ -257,7 +203,7 @@
 			series4.Name = "算术平均滤波法";
 			series4.View = lineSeriesView4;
 			chart.SeriesSerializable = (new DevExpress.XtraCharts.Series[] { series1, series2, series3, series4 });
-			chart.Size = new System.Drawing.Size(826, 478);
+			chart.Size = new System.Drawing.Size(826, 503);
 			chart.TabIndex = 4;
 			chartTitle1.Alignment = System.Drawing.StringAlignment.Near;
 			chartTitle1.DXFont = new DevExpress.Drawing.DXFont("Microsoft YaHei", 18F);
@@ -268,10 +214,6 @@
 			// 
 			xtraOpenFileDialog.FileName = "xtraOpenFileDialog";
 			xtraOpenFileDialog.Filter = "数据文件(*.dat)|*.dat";
-			// 
-			// xtraSaveFileDialog
-			// 
-			xtraSaveFileDialog.Filter = "pdf|*pdf|xls|*xls|xlsx|*xlsx|docx|*docx";
 			// 
 			// Form
 			// 
@@ -306,24 +248,17 @@
 
 		private DevExpress.XtraBars.BarManager barManager1;
 		private DevExpress.XtraBars.Bar bar1;
-		private DevExpress.XtraBars.Bar bar2;
 		private DevExpress.XtraBars.Bar bar3;
 		private DevExpress.XtraBars.BarDockControl barDockControlTop;
 		private DevExpress.XtraBars.BarDockControl barDockControlBottom;
 		private DevExpress.XtraBars.BarDockControl barDockControlLeft;
 		private DevExpress.XtraBars.BarDockControl barDockControlRight;
-		private DevExpress.XtraBars.BarSubItem barSubItem1;
-		private DevExpress.XtraBars.BarSubItem barSubItem3;
-		private DevExpress.XtraBars.BarButtonItem OpenHistoryFile;
-		private DevExpress.XtraBars.BarButtonItem barButtonItem5;
 		private DevExpress.XtraCharts.ChartControl chart;
 		private DevExpress.XtraEditors.XtraOpenFileDialog xtraOpenFileDialog;
-		private DevExpress.XtraBars.BarSubItem barSubItem5;
 		private DevExpress.XtraBars.BarStaticItem statusText;
 		private DevExpress.XtraBars.BarButtonItem Preview;
-		private DevExpress.XtraBars.BarButtonItem Export;
-		private DevExpress.XtraEditors.XtraSaveFileDialog xtraSaveFileDialog;
 		private DevExpress.XtraBars.BarButtonItem Paramters;
+		private DevExpress.XtraBars.BarButtonItem openData;
 	}
 }
 
--- W0_0W._202300226.DataAnalysis/Form.cs
+++ W0_0W._202300226.DataAnalysis/Form.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Reactive.Disposables;
 using System.Windows.Forms;
-using DevExpress.XtraCharts.Printing;
 using DevExpress.XtraEditors;
-using DevExpress.XtraPrinting;
 using DevExpress.XtraReports.UI;
 using DevExpress.XtraSplashScreen;
 using Splat;
 using W0_0W._202300226.DataAnalysis.Model;
-using PrintImageFormat = DevExpress.XtraCharts.Printing.PrintImageFormat;
 
 namespace W0_0W._202300226.DataAnalysis;
 
@@ -21,11 +17,11 @@ public partial class Form : XtraForm
 		InitializeComponent();
 	}
 
-	SignalFactory signalFactory;
+	SignalFactory _signalFactory;
 	/// <summary>
 	/// 打开历史数据
 	/// </summary>
-	void OpenHistoryFile_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+	void OpenData_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
 	{
 		//文件对话框，只有选中文件，才继续
 		if (xtraOpenFileDialog.ShowDialog(this) == DialogResult.OK)
@@ -37,17 +33,17 @@ public partial class Form : XtraForm
 			using (ShowProgress())
 			{
 				//获取一个信号量工厂
-				signalFactory = Locator.Current.GetService<SignalFactory>();
+				_signalFactory = Locator.Current.GetService<SignalFactory>();
 				//加载数据
-				signalFactory.Load(fileName);
+				_signalFactory.Load(fileName);
 
-				SetSeries("信号量", signalFactory.Signals);
-				SetSeries("限幅滤波法", signalFactory.LimitingSignalFilterResult);
-				SetSeries("中位值滤波法", signalFactory.MedianSignalFilterResult);
-				SetSeries("算术平均滤波法", signalFactory.AverageSignalFilterResult);
+				SetSeries("信号量", _signalFactory.Signals);
+				SetSeries("限幅滤波法", _signalFactory.LimitingSignalFilterResult);
+				SetSeries("中位值滤波法", _signalFactory.MedianSignalFilterResult);
+				SetSeries("算术平均滤波法", _signalFactory.AverageSignalFilterResult);
 
 				//在chart标题中显示设备号和峰值
-				chart.Titles[0].Text = $"#{signalFactory.DeviceName} - 峰值: {signalFactory.MaxValue}";
+				chart.Titles[0].Text = $"#{_signalFactory.DeviceName} - 峰值: {_signalFactory.MaxValue}";
 				//状态栏显示历史文件的路径
 				statusText.Caption = fileName;
 			}
@@ -69,55 +65,11 @@ public partial class Form : XtraForm
 	//打印预览
 	void Preview_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
 	{
-		var report = new Report();
-		report.objectDataSource1.DataSource = signalFactory;
-		report.ShowPreviewDialog();
-		//chart.ShowPrintPreview();
-	}
-
-	//导出
-	void Export_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
-	{
-		//文件保存对话框，只有添文件名才能继续操作
-		if (xtraSaveFileDialog.ShowDialog() == DialogResult.OK)
+		if (_signalFactory != null)
 		{
-			//缩放模式
-			chart.OptionsPrint.SizeMode = PrintSizeMode.Zoom;
-			//获取文件路径
-			var filename = xtraSaveFileDialog.FileName;
-			//获取文件扩展名
-			var extension = Path.GetExtension(filename);
-			using (ShowProgress())
-			{
-				switch (extension)
-				{
-					//导出PDF
-					case ".pdf":
-						{
-							chart.OptionsPrint.ImageFormat = PrintImageFormat.Metafile;
-							var options = new PdfExportOptions
-							{
-								ConvertImagesToJpeg = false
-							};
-
-							chart.ExportToPdf(filename, options);
-
-							break;
-						}
-					//导出XLS
-					case ".xls":
-						chart.ExportToXls(filename);
-						break;
-					//导出XLSX
-					case ".xlsx":
-						chart.ExportToXlsx(filename);
-						break;
-					//导出DOCX
-					case ".docx":
-						chart.ExportToDocx(filename);
-						break;
-				}
-			}
+			var report = new Report();
+			report.objectDataSource1.DataSource = _signalFactory;
+			report.ShowPreviewDialog();
 		}
 	}
 
